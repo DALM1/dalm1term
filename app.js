707fb99d702a569471afcdcc954531edd7402c8f
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-04-04 05:01:07 +0200

    Updates

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from 'express';
 import { exec } from 'child_process';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get('/execute', (req, res) => {
   const command = req.query.command;
 
-  exec(command, (error, stdout, stderr) => {
+  exec(command, { shell: true }, (error, stdout, stderr) => {
     if (error) {
       res.send(stderr);
     } else {
